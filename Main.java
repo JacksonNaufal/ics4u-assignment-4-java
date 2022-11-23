2212a12a322f268e3479e6fd1a1fcd58cc30e1db
Jackson-Naufal <Jackson.naufal@mths.ca>
2022-11-23 14:45:46 +0000

    fixes

--- Main.java
+++ Main.java
@@ -4,17 +4,16 @@
 *
 * @author  Jackson Naufal
 * @version 1.0
-* @since   2020-11-08
+* @since   2020-11-23
 *
-* This is a hour glass program
+* This is a hourglass program
 */
-
 import java.util.Scanner;
+
 /**
  * This is the main hour glass.
  * Class Main
  */
-
 final class Main {
 
     /**
@@ -39,45 +38,53 @@ final class Main {
      */
     static void hourglass(final int sand, final int mid) {
 
+        // These are the variables
         String hrGlass = "";
-        int counter1;
-        int counter2;
 
-        for (counter1 = 0; counter1 < mid; counter1++) {
+        // This is the loop for the blankspace
+        for (int counter1 = 0; counter1 < mid; counter1++) {
             hrGlass += " ";
         }
 
-        for (counter2 = 0; counter2 < sand; counter2++) {
+        // This is where the * are placed
+        for (int counter2 = 0; counter2 < sand; counter2++) {
             hrGlass += "* ";
         }
         System.out.println(hrGlass);
 
+        // recursive formula
         if (sand > 1) {
             hourglass(sand - 1, mid + 1);
         }
         System.out.println(hrGlass);
     }
+
     /**
     * The starting main() function.
     *
     * @param args No args will be used
     */
-
     public static void main(final String[] args) {
 
+        // Scanner for first input.
         final Scanner firstInput = new Scanner(System.in);
-        try {
-            System.out.println("This is an hour glass program!");
-            System.out.print("Enter your size: ");
-            final int amount = firstInput.nextInt();
 
+        //What the program is and Input.
+        System.out.println("This is an hourglass program!");
+        System.out.print("Enter your amount: ");
+
+        try {
+            final int amount = firstInput.nextInt();
+            System.out.println("");
+
+            // Checks users validity.
             if (amount <= 0) {
-                System.out.println("Invalid Input, Number to small!");
+                System.out.println("Invalid Input, Number is to small!");
             } else {
                 hourglass(amount, 0);
             }
         } catch (java.util.InputMismatchException ex) {
-            System.out.println("Invalid Input");
+            System.out.println("\nInvalid Input");
         }
         System.out.println("\nDone!");
     }
